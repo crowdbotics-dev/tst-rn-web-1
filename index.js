d8039346b8521ee60fa27d5c7b2d5571e6fbf2ed
Saurav <sp.gharti@gmail.com>
2022-12-19 20:49:57 +0545

    mf

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
+import { AppRegistry } from "react-native"
+import App from "./App"
+import { name as appName } from "./app.json"
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
 
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById("root")
-});
+if (window.document) {
+  AppRegistry.runApplication(appName, {
+    rootTag: document.getElementById("root")
+  })
+}
